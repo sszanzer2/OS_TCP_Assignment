cf595b4139beb509d99c4ff17ef33de663a71c5f
sszanzer2 <122063650+sszanzer2@users.noreply.github.com>
2023-10-31 20:28:08 -0400

    fixed received resent messages

--- ClientTest.java
+++ ClientTest.java
@@ -9,11 +9,10 @@ public class ClientTest2 {
         try (Socket socket = new Socket("localhost", 12349);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
-            System.out.println("Connected to server.");
+            System.out.println("Connected to the server.");
 
             Map<Integer, String> receivedPackets = new HashMap<>();
-            ArrayList<Integer> missingPackets = new ArrayList<>();
-            ArrayList<String> words = new ArrayList<>();
+            List<Integer> missingPackets = new ArrayList<>();
             int totalPackets = 20;
 
             long startTime = System.currentTimeMillis();
@@ -26,77 +25,87 @@ public class ClientTest2 {
                     if (inputLine != null) {
                         System.out.println("Received: " + inputLine);
                         int packetNumber = extractPacketNumber(inputLine);
-                        words.add(inputLine);
-                        receivedPackets.put(packetNumber,inputLine);
+                        receivedPackets.put(packetNumber, inputLine);
                     }
                 } else {
                     // No data available, wait for a short time before checking again
                     Thread.sleep(50); // Sleep for 50 milliseconds (adjust as needed)
                 }
             }
+
             if (receivedPackets.size() == totalPackets) {
                 System.out.println("All packets have been received.");
+
+                // Sort and print the received packets by the numbers in the packets
+                List<Map.Entry<Integer, String>> sortedPackets = new ArrayList<>(receivedPackets.entrySet());
+                sortedPackets.sort(Comparator.comparingInt(entry -> extractNumberInPacket(entry.getValue())));
+
+                for (Map.Entry<Integer, String> entry : sortedPackets) {
+                    int packetNumber = entry.getKey();
+                    String packetData = entry.getValue();
+                    System.out.println("Packet with number " + packetNumber + " received: " + packetData);
+                }
             } else {
-            	System.out.println("Not all packets were received.");
+                System.out.println("Not all packets were received.");
                 for (int i = 1; i <= totalPackets; i++) {
                     if (!receivedPackets.containsKey(i)) {
                         missingPackets.add(i);
                         System.out.print("Missing: " + i + "|" + totalPackets + "\n");
                     }
                 }
-                List<String> receivedWords = new ArrayList<>(Collections.nCopies(totalPackets, ""));
-
-             // Handle missing packet responses from the server
+                // Handle missing packet responses from the server
                 while (!missingPackets.isEmpty()) {
-                	// Request missing packets from the server
-                    for (int missingPacket : missingPackets) {                    	
+                    // Request missing packets from the server
+                    for (int missingPacket : missingPackets) {
                         out.println("REQUEST:" + missingPacket);
                         System.out.println("REQUEST:" + missingPacket);
                     }
-                    String response = in.readLine();
-                    if (response.startsWith("MISSING:")) {
-                    	String[] parts = response.substring("MISSING:".length()).split("\\|");
-                    	 if (parts.length >= 2) {
-                    	        
-                    	    int packetNumber = Integer.parseInt(parts[0]);
+                    String response;
+                    while ((response = in.readLine()) != null && response.startsWith("MISSING:")) {
+                        String[] parts = response.substring("MISSING:".length()).split("\\|");
+                        if (parts.length >= 2) {
+                            int packetNumber = Integer.parseInt(parts[0]);
                             String packetData = parts[1];
-                            int index = Integer.parseInt(parts[2]);
-                            receivedWords.set(index, packetData);
                             receivedPackets.put(packetNumber, packetData);
-                            missingPackets.remove((Integer)packetNumber);
-                            System.out.println("Received resent packet: " + packetNumber + " " + packetData); // Print the resent packet
-                        //}
-                    }
+                            missingPackets.remove((Integer) packetNumber);
+                            System.out.println("Received resent packet: " + packetNumber + " " + packetData);
+                        }
+                    
                 
-
-                // Check if all packets have been received after handling missing packets
+                
                 if (receivedPackets.size() == totalPackets) {
-                    for (int i = 1; i <= totalPackets; i++) {
-                        System.out.println("Packet " + i + " recieved. " );
-                       
-                    }
-                    System.out.println("All packets were recieved!");
-                 // Sort and print the received words based on the index
-                    Collections.sort(receivedWords, Comparator.comparingInt(Integer::parseInt));
-
-                    for (String receivedPacket : receivedWords) {
-                        System.out.print(receivedPacket + " ");
-                    }
-
-
+                    System.out.println("All packets have been received.");
+                    break;
+                	}
                 }
+                    List<Map.Entry<Integer, String>> sortedPackets = new ArrayList<>(receivedPackets.entrySet());
+                    sortedPackets.sort(Comparator.comparingInt(entry -> extractNumberInPacket(entry.getValue())));
 
-                    }    }}  
 
+
+                    for (Map.Entry<Integer, String> entry : sortedPackets) {
+                        System.out.print(entry.getValue() + " ");
+                    }
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
     private static int extractPacketNumber(String packet) {
         String[] parts = packet.split("\\|");
         if (parts.length >= 1) {
+            return Integer.parseInt(parts[0]);
+        }
+        return -1;
+    }
+
+    private static int extractNumberInPacket(String packet) {
+        String[] parts = packet.split(" ");
+        if (parts.length >= 2) {
             return Integer.parseInt(parts[0]);
         }
         return -1;
