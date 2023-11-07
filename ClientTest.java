518d7f6fd90fb4649e4b5b967617adb70a584abc
sszanzer2 <122063650+sszanzer2@users.noreply.github.com>
2023-11-06 21:53:01 -0500

    debugged missingPackets and fixed the display sentence at the end

--- ClientTest.java
+++ ClientTest.java
@@ -3,6 +3,7 @@ package ss;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class Client {
 	public static void main(String[] args) {
@@ -12,7 +13,9 @@ public class Client {
             System.out.println("Connected to the server.");
 
             Map<Integer, String> receivedPackets = new HashMap<>();
+            Map<Integer, String> sortedPackets = new HashMap<>();
             List<Integer> missingPackets = new ArrayList<>();
+            String inputLine= " ";
             int totalPackets = 20;
 
             long startTime = System.currentTimeMillis();
@@ -21,38 +24,55 @@ public class Client {
             while (System.currentTimeMillis() - startTime < timeout) {
                 // Check if there's data available for reading
                 if (in.ready()) {
-                    String inputLine = in.readLine();
+                    inputLine = in.readLine();
                     if (inputLine != null) {
                         System.out.println("Received: " + inputLine);
-                        int packetNumber = extractNumberInPacket(inputLine);
+                        int packetIndex = extractNumberInPacket(inputLine);
+                        int packetNumber = extractPacketNumber(inputLine);
                         receivedPackets.put(packetNumber, inputLine);
+                        sortedPackets.put(packetIndex, inputLine);
+                        
                     }
+                   
                 } else {
                     // No data available, wait for a short time before checking again
                     Thread.sleep(50); // Sleep for 50 milliseconds (adjust as needed)
                 }
             }
+            
+           /* for(Entry<Integer, String> i: receivedPackets.entrySet()) {
+            	System.out.println("String : " + i.getValue() + " Number: " + i.getKey());
+            }*/
 
             if (receivedPackets.size() == totalPackets) {
                 System.out.println("All packets have been received.");
-
-                // Sort and print the received packets by the numbers in the packets
-                List<Map.Entry<Integer, String>> sortedPackets = new ArrayList<>(receivedPackets.entrySet());
-                sortedPackets.sort(Comparator.comparingInt(entry -> extractNumberInPacket(entry.getValue())));
-
-                for (Map.Entry<Integer, String> entry : sortedPackets) {
-                    int packetNumber = entry.getKey();
-                    String packetData = entry.getValue();
-                    System.out.println("Packet with number " + packetNumber + " received: " + packetData);
+                
+                
+                for(int i = 1; i<receivedPackets.size()+1; i++) {
+                	if(receivedPackets.containsKey(i)) {
+                		System.out.print(receivedPackets.get(i) + " ");
+                	}
                 }
+              
             } else {
-                System.out.println("Not all packets were received.");
-                for (int i = 1; i <= totalPackets; i++) {
-                    if (!receivedPackets.containsKey(i)) {
-                        missingPackets.add(i);
-                        System.out.print("Missing: " + i + "|" + totalPackets + "\n");
+                /*System.out.println("Not all packets were received.");
+                for (int x = 1; x <= totalPackets; x++) {
+                    if (!receivedPackets.containsKey(x)) {
+                    	String[] packetNum = inputLine.split(" ");
+                    	int packetNumber = Integer.parseInt(packetNum[0]);
+                        missingPackets.add(packetNumber);
+                        System.out.print("Missing: " + packetNumber + "|" + totalPackets + "\n");
                     }
-                }
+                
+                }*/
+            	
+            	System.out.println("Not all packets were received.");
+            	for (int x = 1; x <= totalPackets; x++) {
+            	    if (!receivedPackets.containsKey(x)) {
+            	        missingPackets.add(x);
+            	        System.out.println("Missing: " + x + "|" + totalPackets);
+            	    }
+            	}
                 // Handle missing packet responses from the server
                 while (!missingPackets.isEmpty()) {
                     // Request missing packets from the server
@@ -66,15 +86,17 @@ public class Client {
                         //for(int i = 0; i<parts.length;i++)
                        // System.out.print(parts[0] + " ");
                     	String packetNumber = parts[0];
+                    	//System.out.println(packetNumber);
                         String packetData = parts[1];
                         int index = Integer.parseInt(parts[2]);
                         receivedPackets.put(index, packetData);
-                        String [] packetNumberParts = packetNumber.split("|");
+                        String [] packetNumberParts = packetNumber.split("\\|");
+                        //throwing exception
                         missingPackets.remove(Integer.parseInt(packetNumberParts[0]));
                         System.out.println("Received resent packet: " + packetNumber + " " + packetData + " " + index);
                        
                     
-                
+                    
                 
                 if (receivedPackets.size() == totalPackets) {
                     System.out.println("All packets have been received.");
@@ -84,10 +106,10 @@ public class Client {
                     //List<Map.Entry<Integer, String>> sortedPackets = new ArrayList<>(receivedPackets.entrySet());
                     //sortedPackets.sort(Comparator.comparingInt(entry -> extractNumberInPacket(entry.getValue())));
                     
-                    for(int i = 1; i<receivedPackets.size()+1; i++) {
-                    	if(receivedPackets.containsKey(i)) {
-                    		System.out.print(receivedPackets.get(i) + " ");
-                    	}
+                    for (int i = 1; i <= totalPackets; i++) {
+                        if (sortedPackets.containsKey(i)) {
+                            System.out.print(sortedPackets.get(i) + " ");
+                        }
                     }
 
                   /*  int index = 1;
@@ -95,6 +117,8 @@ public class Client {
               
                         System.out.print(entry.getValue() + " ");
                     }*/
+                
+            
                 }
             }
         } catch (IOException e) {
@@ -104,8 +128,8 @@ public class Client {
         }
     }
 
-    private static int extractPacketNumber(String packet) {
-        String[] parts = packet.split(" ");
+   private static int extractPacketNumber(String packet) {
+        String[] parts = packet.split("\\|");
         if (parts.length >= 1) {
             return Integer.parseInt(parts[0]);
         }
